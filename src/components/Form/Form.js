24d360f20343b52560c52e74e4f2a1d0e985fc82
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-01 19:46:40 +0600

    designed the education section

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Personal from './Personal';
+import Experience from './Experience';
 
 export default function Form(props) {
 	return (
 		<form className='cv-form'>
 			<Personal />
+			<Experience />
 		</form>
 	);
 }
