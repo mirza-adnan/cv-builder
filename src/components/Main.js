622ca0d318bf592218d8490608407a77f75bfcd4
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-06 19:21:25 +0600

    added appropriate properties to state objects

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,37 @@ export class Main extends Component {
 	constructor() {
 		super();
 		this.state = {
-			personal: {},
-			education: [],
-			experience: [],
+			personal: {
+				firstName: '',
+				lastName: '',
+				jobTitle: '',
+				phoneNumber: '',
+				email: '',
+				address: '',
+				description: '',
+			},
+			education: [
+				{
+					institute: '',
+					city: '',
+					degree: '',
+					subject: '',
+					from: '',
+					to: '',
+				},
+			],
+			experience: [
+				{
+					position: '',
+					company: '',
+					city: '',
+					from: '',
+					to: '',
+				},
+			],
 		};
 	}
+
 	render() {
 		return (
 			<main>
