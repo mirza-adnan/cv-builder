00c7f09dd90dd38e3c3ebd540e2a40738987aeea
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-07 22:11:37 +0600

    designed the preview header

---
+++ src/components/Preview/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<div className='prev-header'>
+			<div className='header-left'>
+				<h2>Mirza Adnan</h2>
+				<p>Jr. Developer</p>
+			</div>
+			<div className='header-right'>
+				<p>Phone: 01610210210,</p>
+				<p>mirza.adnan2205@gmail.com</p>
+				<p>Uttara, Dhaka</p>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
