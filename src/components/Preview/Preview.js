fb549353fb69124d7d2a1234708cfe4ff7d2574c
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-09 15:54:43 +0600

    added individual components

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import PrevPersonal from './PrevPersonal';
+import EducationBlock from './EducationBlock';
+import ExperienceBlock from './ExperienceBlock';
 
 export class Preview extends Component {
 	render() {
@@ -8,7 +9,25 @@ export class Preview extends Component {
 			<div className='preview'>
 				<Header />
 				<div className='preview-main'>
-					<PrevPersonal />
+					<section className='preview-section'>
+						<h3>Personal Description</h3>
+						<p className='description'>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
+							esse, pariatur necessitatibus ratione quas laboriosam sit in nam
+							architecto sapiente?
+						</p>
+					</section>
+					<section className='preview-section'>
+						<h3>Education</h3>
+						<EducationBlock />
+						<EducationBlock />
+					</section>
+					<section className='preview-section'>
+						<h3>Experience</h3>
+						<ExperienceBlock />
+						<ExperienceBlock />
+						<ExperienceBlock />
+					</section>
 				</div>
 			</div>
 		);
