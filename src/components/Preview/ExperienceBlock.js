1dd2899fdb29a083839c23666fd39005fd1affad
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-09 15:54:13 +0600

    finished design

---
+++ src/components/Preview/ExperienceBlock.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ExperienceBlock() {
+	return (
+		<div className='edu-block block'>
+			<div className='time-range'>2002-2006</div>
+			<div className='block-content'>
+				<p>Developer</p>
+				<p>XYZ Inc,</p>
+				<p>ABC, HJK</p>
+			</div>
+		</div>
+	);
+}
+
+export default ExperienceBlock;
