d1d93044fb0d97c921feb92a8c9e93ed92a1fe23
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-09 15:54:32 +0600

    changed the block design a bit

--- src/components/Preview/EducationBlock.js
+++ src/components/Preview/EducationBlock.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 function EducationBlock() {
 	return (
-		<div className='edu-block block'>
+		<div className='exp-block block'>
 			<div className='time-range'>2002-2006</div>
-			<div className='block-wrapper'>
-				<div className='block-content'>
-					<p>ABC University of XYZ</p>
-					<p>City, Province</p>
-					<p>Degree: Degree</p>
-					<p>Subject: Subject</p>
-				</div>
+			<div className='block-content'>
+				<p>ABC University of XYZ</p>
+				<p>City, Province</p>
+				<p>Degree: Degree</p>
+				<p>Subject: Subject</p>
 			</div>
 		</div>
 	);
