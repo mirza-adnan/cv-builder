d5dbb233a4d3e11540280051b78d9c020992eb63
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-09 22:57:33 +0600

    removed ids

--- src/components/Form/Personal.js
+++ src/components/Form/Personal.js
@@ -7,73 +7,68 @@ export default function Personal(props) {
 			<h2>Personal Information</h2>
 			<div className='double-field'>
 				<div className='field'>
-					<label htmlFor='firstName'>First Name</label>
+					<label>First Name</label>
 					<input
 						type='text'
 						name='firstName'
 						className='text-input'
 						placeholder='First Name'
 						autoComplete='off'
-						id='firstName'
 						value={personal.firstName}
 						onChange={handleChange}
 					/>
 				</div>
 				<div className='field'>
-					<label htmlFor='lastName'>Last Name</label>
+					<label>Last Name</label>
 					<input
 						type='text'
 						name='lastName'
 						className='text-input'
 						placeholder='Last Name'
 						autoComplete='off'
-						id='lastName'
 						value={personal.lastName}
 						onChange={handleChange}
 					/>
 				</div>
 			</div>
 			<div className='field'>
-				<label htmlFor='jobTitle'>Job Title</label>
+				<label>Job Title</label>
 				<input
 					type='text'
 					name='jobTitle'
 					className='text-input'
 					placeholder='Job Title'
 					autoComplete='off'
-					id='jobTitle'
 					value={personal.jobTitle}
 					onChange={handleChange}
 				/>
 			</div>
 			<div className='field'>
-				<label htmlFor='phoneNumber'>Phone Number</label>
+				<label>Phone Number</label>
 				<input
 					type='text'
 					name='phoneNumber'
 					className='text-input'
 					placeholder='Phone Number'
 					autoComplete='off'
-					id='phoneNumber'
 					value={personal.phoneNumber}
 					onChange={handleChange}
 				/>
 			</div>
 			<div className='field'>
-				<label htmlFor='email'>Email</label>
+				<label>Email</label>
 				<input
 					type='email'
 					name='email'
 					className='text-input'
 					placeholder='Email'
 					autoComplete='off'
-					id='email'
 					value={personal.email}
 					onChange={handleChange}
 				/>
 			</div>
 			<div className='field'>
-				<label htmlFor='address'>Address</label>
+				<label>Address</label>
 				<input
 					type='text'
 					name='address'
@@ -86,10 +81,9 @@ export default function Personal(props) {
 				/>
 			</div>
 			<div className='field'>
-				<label htmlFor='description'>Description</label>
+				<label>Description</label>
 				<textarea
 					name='description'
-					id='description'
 					cols='30'
 					rows='6'
 					placeholder='Describe yourself briefly'
