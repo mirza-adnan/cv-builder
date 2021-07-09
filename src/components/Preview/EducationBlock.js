1796b8f50aea800625af9d5d0492026c3d64e3a9
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-09 15:29:36 +0600

    designed education section of preview

---
+++ src/components/Preview/EducationBlock.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function EducationBlock() {
+	return (
+		<div className='edu-block block'>
+			<div className='time-range'>2002-2006</div>
+			<div className='block-wrapper'>
+				<div className='block-content'>
+					<p>ABC University of XYZ</p>
+					<p>City, Province</p>
+					<p>Degree: Degree</p>
+					<p>Subject: Subject</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default EducationBlock;
